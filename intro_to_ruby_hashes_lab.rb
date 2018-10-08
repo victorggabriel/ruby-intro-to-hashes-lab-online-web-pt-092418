560040f5645cb6ccf58eac7a49b1280f8922dfbd
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 19:38:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,6 @@ end
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly[:railroads][:pieces] = 4
-	monopoly
 end
 
 def monopoly_with_third_tier
