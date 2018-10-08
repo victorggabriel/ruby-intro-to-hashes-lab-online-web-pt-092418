6e84b799c96fe7076f359a4deb7a58dd715fee64
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 19:49:57 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,7 @@ def monopoly_with_fourth_tier
 			:pieces => 4,
 			:names => {
 				:reading_railroad => { "mortgage_value" => "$100"},
-				:pennsylvania_railroad => {},
+				:pennsylvania_railroad => {"mortgage_value" => "$100"},
 				:b_and_o_railroad => {},
 				:shortline => {}
 			},
